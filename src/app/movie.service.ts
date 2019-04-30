0a7f8c2f5fd46b94a9a7c36ce8701a315e491a28
Notorious <notoriousbpp@gmail.com>
2019-04-30 06:27:22 -0700

    docs commit

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import {OmdbResult} from './omdb-result';
 import {Movie} from './movie';
 
